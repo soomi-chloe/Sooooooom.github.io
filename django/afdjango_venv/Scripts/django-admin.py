53dd6cb3ddb4fa3a027637d2e0e2b912e789ad13
Soomi Jang <109soomi@naver.com>
2020-04-05 19:24:39 +0900

    for django

---
+++ django/afdjango_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\109so\OneDrive\문서\website-example\django\afdjango_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
